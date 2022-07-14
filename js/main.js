26134245122132afc2fb391baa5450892fa9d7fe
Alhassan Mohamed <86925987+Mhmh-M@users.noreply.github.com>
2022-07-15 01:44:14 +0200

    add project

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// let body = document.querySelector("body")
+// for (let i = 0; i < day_data.length; i++) {
+//   let ele = document.createElement('div')
+//   ele.innerHTML = day_data[i]['zakr']
+//   body.appendChild(ele)
+
+// }
